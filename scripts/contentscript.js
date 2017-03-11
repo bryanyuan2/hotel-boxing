91ff3146a7ebbabe8309e0abf91e45db884cdaa5
bryanyuan2 <bryanyuan2@gmail.com>
2017-03-11 11:50:45 +0800

    updated

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -8,15 +8,39 @@ $(document).ready(function() {
     // get resource
     // var resource = chrome.extension.getURL("audio/test.mp3");
 
+    var comments = ['great room', 'this is a awesome trip', 'I think the room service is not very well'];
+
+
+    var dom = document.createElement('div'),
+    	getBookingBlur = '.booking-blur',
+    	getBookingList = '.booking-list',
+    	getBookingSel = '.sr_item';
+    var hotelID = '';
+    $(dom).addClass('booking-blur');
+
+	$("body").append(dom);
+	$(getBookingBlur).hide(); 
+
+	$(getBookingSel).mouseenter(function(){
+		$(getBookingBlur).show();
+		hotelID = $(this).find('.sr_item_photo').attr('id').replace('hotel_', '');
+		console.log("hotelID", hotelID);
+
+	}).mouseleave(function(){
+		$(getBookingBlur).hide();
+	});
+
+    for (var i=0;i<comments.length;i++) {
+    	var commentList = document.createElement('div');
+    	$(commentList).addClass('booking-list').text(comments[i]);
+    	$(getBookingBlur).append(commentList);
+    }
+
     // create dom
     var dom = document.createElement("div");
-    $(dom).addClass("booking-blur").text('test');
     $("body").append(dom);
 
     lib = new BDCLib();
     lib.debug();
 });
 })();
-
-
-
