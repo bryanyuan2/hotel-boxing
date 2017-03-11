fef35fa5e670ffe22adac02ace6f7993908dd279
nlin <nlin@yahoo-inc.com>
2017-03-11 15:04:38 +0800

    Solve conflict

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -36,11 +36,71 @@ $(document).ready(function() {
     	$(getBookingBlur).append(commentList);
     }
 
-    // create dom
-    var dom = document.createElement("div");
-    $("body").append(dom);
+    var pktableContainer = document.createElement("div");
+    $(pktableContainer).attr("id", "pktable-container");
+    
+    var pktable =   "<table class='booking-pktable'>" +
+                        "<tbody>" +
+                            "<tr>" +
+                                "<td>Hotel</td>" +
+                                "<td><img src='http://t-ec.bstatic.com/images/hotel/square200/392/39242578.jpg' alt=''></td>" +
+                                "<td><img src='http://s-ec.bstatic.com/images/hotel/square200/728/72839775.jpg' alt=''></td>" +
+                                "<td><img src='http://s-ec.bstatic.com/images/hotel/square200/360/36046638.jpg' alt=''></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                                "<td>breakfast</td>" +
+                                "<td></td>" +
+                                "<td></td>" +
+                                "<td></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                                "<td>parking</td>" +
+                                "<td></td>" +
+                                "<td></td>" +
+                                "<td></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                                "<td>pool</td>" +
+                                "<td></td>" +
+                                "<td></td>" +
+                                "<td></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                                "<td><input type='text' id='booking-input-kw' placeholder='Feature:'/><i id='booking-add-kw-button' class='fa fa-plus-square' aria-hidden='true'></i></td>" +
+                                "<td></td>" +
+                                "<td></td>" +
+                                "<td></td>" +
+                            "</tr>" +
+                        "</tbody>" +
+                    "</table>";
+
+    $(pktableContainer).append(pktable);
+    $("body").append(pktableContainer); 
+
+    $("#booking-add-kw-button").click(function(){
+        addNewRow( $("#booking-input-kw").val() );
+    });
+    $("#booking-add-kw-button").hover(function(){
+        $(this).addClass("fa-lg");
+    }, function(){
+        $(this).removeClass("fa-lg");
+    });
+
+    $("#booking-input-kw").blur(function(){
+    });
 
     lib = new BDCLib();
     lib.debug();
 });
 })();
+
+function addNewRow (kw) {
+    
+    console.log(kw);
+    var newRow =    "<tr>" +
+                        "<td>" +
+                            "<input type='text' id='input-kw' placeholder='What's important to you...'/>"
+                        "</td>" +
+                    "</tr>";
+    return newRow;
+}
