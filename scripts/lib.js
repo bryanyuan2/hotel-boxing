a9674ce3ddd8adcbb5702debae2b1ceadd5e134a
Chienwen Chen <bitbucket@chienwen.net>
2017-03-11 12:58:05 +0800

    add favorite list function

--- scripts/lib.js
+++ scripts/lib.js
@@ -1,14 +1,17 @@
 function BDCLib(){
-    var _cache = {};
+    var _store = {
+        cache: {},
+        favorite: {}
+    };
     var _getCache = function(url){
-        if(_cache[url]){
-            return _cache[url];
+        if(_store.cache[url]){
+            return _store.cache[url];
         }
         return null;
     };
     var _putCache = function(url, content){
         if(content){
-            _cache[url] = content;
+            _store.cache[url] = content;
         }
     };
     var _debug = function(){
@@ -79,6 +82,41 @@ function BDCLib(){
         //    exports.getHotelInfo('725241', function(data){_debug('get info', data)});
         //    exports.getHoteReviews('725241', function(data){_debug('get reviews', data)});
         //}, 5000);
+        //
+        
+        exports.addFavoriteHotel('725241', function(){
+            _debug('favorite list:', exports.getFavoriteHotels());
+            exports.removeFavoriteHotel('725241');
+            _debug('favorite list:', exports.getFavoriteHotels());
+        });
+    };
+
+    exports.addFavoriteHotel = function(hotelId, done){
+        if(_store.favorite[hotelId]){
+            _debug('warning: duplicate favorite hotel ID ' + hotelId);
+            return false;
+        }
+        _store.favorite[hotelId] = true;
+        exports.getHotelInfo(hotelId, function(data){
+            _store.favorite[hotelId] = data;
+            if(done){
+                done(data);
+            }
+        });
+        return true;
+    };
+    
+    exports.removeFavoriteHotel = function(hotelId){
+        if(!_store.favorite[hotelId]){
+            _debug('warning: not found favorite hotel ID ' + hotelId);
+            return false;
+        }
+        delete _store.favorite[hotelId];
+        return true;
+    };
+    
+    exports.getFavoriteHotels = function(){
+        return _store.favorite;
     };
     
     return exports;
