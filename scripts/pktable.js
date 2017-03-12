bd24e19f6c55f75b1bfd957ec686e62aea7dcb4b
nlin <nlin@yahoo-inc.com>
2017-03-12 10:52:48 +0800

    update comment popup

--- scripts/pktable.js
+++ scripts/pktable.js
@@ -15,35 +15,27 @@ function generatePkTable(){
 
 function commentPopupInit(){
     $.get(chrome.extension.getURL('view/popup-comment.html'), function(data) {
-        $($.parseHTML(data)).appendTo('body');
-        $( ".sectionDom ul" ).on( "click", "li", function() {
-            console.log('popup!!');
-        });
+        $($.parseHTML(data)).appendTo('.pkdom');
     });
 
-    console.log('popup loaded');
-
+    console.log('popup init');
 }
 
-function setCommentPopup(lib){
-    // $(this).parent;
+function setCommentPopup(lib, id, kw, type){
     var id = "733981";
     var kw = "service";
     var type = "pros"; // "pros" or "cons"
 
-    $(".popup-contanier").empty();
+    $(".popup-contanier ul").empty();
     lib.getHotelKeywordReviews(id, kw, function(data){
-        // console.log(data);
         var comments = data.data[kw][type];
         comments.forEach( function(ele, ind) {
             var cn = $(".popup-contanier li").clone(true);
             var comment = ele.text;
             console.log(comment);
             cn.append(comment);
-            $(".popup-contanier").append(cn);
+            $(".popup-contanier ul").append(cn);
         });
 
     });
-    // var cn = $(".popup-contanier li").cloneNode(true);
-
 }
